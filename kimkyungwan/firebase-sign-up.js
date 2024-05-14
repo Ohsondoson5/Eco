19dcb09cc43f7957b91c128a34d6326c4008a91e
OhSeungMok <ahr501@naver.com>
2024-05-14 15:07:49 +0900

    회원가입 경고창 수정

--- kimkyungwan/firebase-sign-up.js
+++ kimkyungwan/firebase-sign-up.js
@@ -39,6 +39,25 @@ const signUp = async (email, password, username) => {
     console.error('회원가입 중 오류 발생:', error.message);
   }
 };
+function showAlertRed(message) {
+  var alertBox = document.createElement('div');
+  alertBox.textContent = message;
+  alertBox.style.backgroundColor = 'red';
+  alertBox.style.fontWeight = 'bold';
+  alertBox.style.border = '1px solid red';
+  alertBox.style.position = 'fixed';
+  alertBox.style.top = '10%';
+  alertBox.style.left = '50%';
+  alertBox.style.transform = 'translate(-50%, -50%)';
+  alertBox.style.padding = '10px 20px';
+  alertBox.style.borderRadius = '5px';
+  alertBox.style.color = 'white';
+  document.body.appendChild(alertBox);
+
+  setTimeout(function() {
+      alertBox.remove();
+  }, 2000); // 2초 후에 다이얼로그가 자동으로 사라지도록 설정
+}
 
 // 이벤트 핸들러
 const signUpButton = document.getElementById('sign-up-btn');
@@ -49,8 +68,43 @@ signUpButton.addEventListener('click', () => {
   const password = document.getElementById('password').value;
   const confirmPassword = document.getElementById('confirm-password').value;
 
+  //아이디가 공백일 경우
+  if (document.getElementById('username').value.trim() === '') {
+    showAlertRed('아이디를 입력해주세요.');
+    document.getElementById('username').focus();
+    return;
+  }
+
+  //이메일 유효성 검사
+  function isValidEmail(email) {
+    // 간단한 이메일 유효성을 위한 정규 표현식
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(email);
+  }
+
+  // 이메일 유효성 검사 후 처리
+  if (!isValidEmail(email)) {
+    showAlertRed('올바른 이메일 주소를 입력해주세요.');
+    document.getElementById('email').focus();
+    return;
+  }
+
+  //비밀번호 검사
+  if (password == '' || password.length < 6) {
+    showAlertRed('비밀번호는 6자 이상이어야 합니다.');
+    document.getElementById('password').focus();
+    return;
+  }
+  //비밀번호 null 일경우
+  if (confirmPassword == '') {
+    showAlertRed('비밀번호를 입력하세요.');
+    document.getElementById('confirm-password').focus();
+    return;
+  }
+
+//비밀번호 일치
   if (password !== confirmPassword) {
-    console.error('비밀번호가 일치하지 않습니다.');
+    showAlertRed('비밀번호가 일치하지 않습니다.');
     return;
   }
 
